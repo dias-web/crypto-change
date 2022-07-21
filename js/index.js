8d74d2c372357ac66dbf6147ed6b1e8255f4fba9
Dias <l1verpoolg24@gmail.com>
2022-07-21 16:43:02 +0600

    feat:completed about-us section and tabs scripts

--- js/index.js
+++ js/index.js
@@ -56,3 +56,66 @@ form.addEventListener("submit", (e) => {
       }, 3000);
     });
 });
+
+//tabs
+
+const advantageButtons = document.querySelectorAll(".advantage__button");
+const advantageItemsContent = document.querySelectorAll(
+  ".advantage__item-content"
+);
+
+advantageButtons.forEach((advantageButton, i) => {
+  advantageButton.addEventListener("click", () => {
+    advantageItemsContent.forEach((advantageItemContent, j) => {
+      if (i === j) {
+        advantageButtons[j].classList.add("advantage__button_active");
+        advantageItemsContent[j].classList.add(
+          "advantage__item-content_active"
+        );
+      } else {
+        advantageButtons[j].classList.remove("advantage__button_active");
+        advantageItemsContent[j].classList.remove(
+          "advantage__item-content_active"
+        );
+      }
+    });
+  });
+});
+
+//exchange
+
+const exchangeRatesList = document.querySelector(".exchange-rates__list");
+
+const socket = new WebSocket("ws://web-socket-current.herokuapp.com");
+
+const renderExchange = (wrapper, data) => {
+  const { from, to, rate, change } = JSON.parse(data);
+
+  const listItem = document.createElement("li");
+  listItem.classList.add(
+    "exchange-rates__item",
+    change === 1 ? "exchange-rates__item_up" : "exchange - rates__item_down"
+  );
+  const currency = document.createElement("span");
+  currency.classList.add("exchange-rates__currency");
+  currency.textContent = `${from}/${to}`;
+
+  const value = document.createElement("span");
+  value.classList.add("exchange-rates__value");
+  value.textContent = rate;
+
+  listItem.append(currency, value);
+  wrapper.prepend(listItem);
+
+  if (wrapper.children.length > 4) {
+    wrapper.children[4].remove();
+  }
+};
+
+socket.addEventListener("message", (event) => {
+  renderExchange(exchangeRatesList, event.data);
+});
+
+socket.addEventListener("error", (err) => {
+  console.log(err);
+});
