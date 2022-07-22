164979894fcc96da6bb686bce8833f19397b7434
Dias <l1verpoolg24@gmail.com>
2022-07-22 15:49:54 +0600

    feat: completed faq section and accordion

--- js/index.js
+++ js/index.js
@@ -7,6 +7,12 @@ headerNavBtn.addEventListener("click", () => {
   headerNavigation.classList.toggle("header__navigation_open");
 });
 
+headerNavigation.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("navigation__link")) {
+    headerNavigation.classList.remove("header__navigation_open");
+  }
+});
+
 //modal
 
 const heroBtn = document.querySelector(".hero__button");
@@ -94,7 +100,7 @@ const renderExchange = (wrapper, data) => {
   const listItem = document.createElement("li");
   listItem.classList.add(
     "exchange-rates__item",
-    change === 1 ? "exchange-rates__item_up" : "exchange - rates__item_down"
+    change === 1 ? "exchange-rates__item_up" : "exchange-rates__item_down"
   );
   const currency = document.createElement("span");
   currency.classList.add("exchange-rates__currency");
@@ -119,3 +125,62 @@ socket.addEventListener("message", (event) => {
 socket.addEventListener("error", (err) => {
   console.log(err);
 });
+
+//faq
+
+const hide = (elem, answer) => {
+  if (!elem.classList.contains("faq__item_show")) return;
+
+  answer.style.height = `${answer.offsetHeight}px`;
+  answer.offsetHeight;
+  answer.style.display = "block";
+  answer.style.height = 0;
+  answer.style.overflow = "hidden";
+  answer.style.transition = "height 0.36s ease-in-out";
+  elem.classList.remove("faq__item_show");
+
+  setTimeout(() => {
+    answer.style.display = "";
+    answer.style.height = "";
+    answer.style.overflow = "";
+    answer.style.transition = "";
+  }, 360);
+};
+
+const show = (elem, answer) => {
+  if (elem.classList.contains("faq__item_show")) return;
+
+  answer.style.display = "block";
+  const height = answer.offsetHeight;
+  answer.style.height = 0;
+  answer.style.overflow = "hidden";
+  answer.style.transition = "height 0.36s ease-in-out";
+  answer.offsetHeight;
+  answer.style.height = `${height}px`;
+
+  setTimeout(() => {
+    elem.classList.add("faq__item_show");
+    answer.style.display = "";
+    answer.style.height = "";
+    answer.style.overflow = "";
+    answer.style.transition = "";
+  }, 360);
+};
+
+const accordion = () => {
+  const list = document.querySelector(".faq__list");
+
+  list.addEventListener("click", (e) => {
+    const button = e.target.closest(".faq__question");
+
+    if (button) {
+      const item = button.closest(".faq__item");
+      const answer = item.querySelector(".faq__answer");
+      item.classList.contains("faq__item_show")
+        ? hide(item, answer)
+        : show(item, answer);
+    }
+  });
+};
+
+accordion();
